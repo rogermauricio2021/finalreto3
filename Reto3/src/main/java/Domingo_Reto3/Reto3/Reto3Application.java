b5fd2cf0a4ce224d7eb875fef5b5f04e6d7fe166
rogermauricio2021 <92679307+rogermauricio2021@users.noreply.github.com>
2021-10-17 16:00:16 -0500

    Add files via upload

---
+++ Reto3/src/main/java/Domingo_Reto3/Reto3/Reto3Application.java
@@ -0,0 +1,14 @@
+
+package Domingo_Reto3.Reto3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Reto3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Reto3Application.class, args);
+	}
+
+}
